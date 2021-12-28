ff0f4d65eb53a034a41fb17abc719a24f683fb1f
gyzeng <gyzeng@adjacentech.com>
2021-12-28 12:32:31 +0800

    ✨ feat: vue3 proxy

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let activeEffect = null
-class Dep{
+class Dep {
     subscriber = new Set()
     depend() {
         if (activeEffect) {
@@ -17,36 +17,52 @@ class Dep{
 function watchEffect(effect) {
     activeEffect = effect
     effect()
-    activeEffect =null
+    activeEffect = null
 }
 
 
+const targetMap = new WeakMap()
+function getDep(target, key) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+        depsMap = new Map()
+        targetMap.set(target, depsMap)
+    }
+
+    let dep = depsMap.get(key)
+    if (!dep) {
+        dep = new Dep()
+        depsMap.set(key, dep)
+    }
+    return dep
+}
+
+const reactiveHandlers = {
+    get(target, key, receiver) {
+        let dep = getDep(target, key)
+        dep.depend()
+        return Reflect.get(target, key, receiver)
+    },
+
+    set(target, key, value) {
+        let dep = getDep(target, key)
+        const result = Reflect.set(target, key, value)
+        dep.notify()
+        return result
+    }
+}
+
 function reactive(raw) {
-    Object.keys(raw).forEach(key => {
-        const dep = new Dep()
-        let value = raw[key]
-
-        Object.defineProperty(raw, key, {
-            get() {
-                dep.depend()
-                return value
-            },
-            set(newValue) {
-                value =newValue
-                dep.notify()
-            }
-        })
-    })
-
-    return raw
+    return new Proxy(raw, reactiveHandlers)
 }
 
 const state = reactive({
-    count:0
+    count: 0
 })
 
 watchEffect(() => {
-    console.log(state.count)
+    console.log(state.msg)
 })
 
 state.count++
+state.msg ="fjjfj"
