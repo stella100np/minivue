f4e592239f2ed5a4de51ea9714e637bdb36d91b9
gyzeng <gyzeng@adjacentech.com>
2021-12-29 15:33:34 +0800

    vue 3 实现

--- main.js
+++ main.js
@@ -1,68 +1,93 @@
 let activeEffect = null
-class Dep {
-    subscriber = new Set()
-    depend() {
-        if (activeEffect) {
-            this.subscriber.add(activeEffect)
-        }
-    }
-
-    notify() {
-        this.subscriber.forEach(effect => {
-            effect()
-        })
-    }
-}
-
-function watchEffect(effect) {
-    activeEffect = effect
-    effect()
+function watchEffect(eff) {
+    activeEffect = eff
+    activeEffect()
     activeEffect = null
 }
 
 
 const targetMap = new WeakMap()
-function getDep(target, key) {
-    let depsMap = targetMap.get(target)
+function tracker(target,key) {
+    if (activeEffect) {
+        let depsMap = targetMap.get(target)
+        if (!depsMap) {
+            targetMap.set(target,(depsMap = new Map()))
+        }
+        let dep = depsMap.get(key)
+        if (!dep) {
+           
+            depsMap.set(key,( dep = new Set()))
+        }
+        dep.add(activeEffect)
+    }
+}
+function trigger(target,key) {
+    const depsMap = targetMap.get(target)
     if (!depsMap) {
-        depsMap = new Map()
-        targetMap.set(target, depsMap)
+        return
     }
-
     let dep = depsMap.get(key)
-    if (!dep) {
-        dep = new Dep()
-        depsMap.set(key, dep)
+    if (dep) {
+        dep.forEach(effect => {effect()})
     }
-    return dep
+
 }
 
-const reactiveHandlers = {
-    get(target, key, receiver) {
-        let dep = getDep(target, key)
-        dep.depend()
-        return Reflect.get(target, key, receiver)
-    },
 
-    set(target, key, value) {
-        let dep = getDep(target, key)
-        const result = Reflect.set(target, key, value)
-        dep.notify()
-        return result
-    }
-}
 
 function reactive(raw) {
-    return new Proxy(raw, reactiveHandlers)
+    return new Proxy(raw, {
+        get(target,key,receiver) {
+            console.log("Get was called with key = "+ key)
+            let result = Reflect.get(target,key,receiver)
+            tracker(target,key)
+            return result
+        },
+        set(target,key,value,receiver) {
+            let oldValue = target[key]
+            console.log("SET was called with key = "+ key+" value = "+value)
+            let result = Reflect.set(target,key,value,receiver)
+            if (oldValue != value) {
+                trigger(target,key)
+            }
+            return result
+        }
+    })
 }
 
-const state = reactive({
-    count: 0
+function ref(raw) {
+    const r = {
+        get value() {
+            tracker(r,'value')
+            return raw
+        },
+        set value(newValue) {
+            raw = newValue
+            trigger(r,'value')
+        }
+    }
+    return r
+}
+
+function computed(getter) {
+    let result = ref()
+    watchEffect(() => {result.value =getter()})
+    return result
+}
+
+
+
+let product = reactive({price:5,quantity:2})
+let salePrice = computed(()=> {
+    return product.price * 0.9
+})
+let total = computed(()=>{
+    return salePrice.value * product.quantity
 })
 
-watchEffect(() => {
-    console.log(state.msg)
-})
 
-state.count++
-state.msg ="fjjfj"
+console.log(`befor updated total 10 = ${total.value}, salePrice (sb 4.5) = ${salePrice.value}`)
+product.quantity =3
+console.log(`after updated total 15 = ${total.value}, salePrice (sb 4.5) = ${salePrice.value}`)
+product.price =10
+console.log(`after2 updated total 30 = ${total.value}, salePrice (sb 9) = ${salePrice.value}`)
